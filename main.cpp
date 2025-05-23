e2b48e27ea2884bece5d478a11749c3edae6802f
Toksyk <167337761+Toksyk@users.noreply.github.com>
2025-05-23 21:51:15 +0200

    implement actual engine into main.cpp

--- main.cpp
+++ main.cpp
@@ -7,7 +7,34 @@
 
 
 #include "lib.h"
+#include <vector>
+#include <cstring> // For memcpy
 
 void testbind(int x) {
     std::cout << "it works, input: " << x << "\n";
+}
+
+void render(const std::vector<std::vector<char>>& game_space) {
+    for(size_t col = 0; col < game_space.size(); ++col) {
+        for(size_t row = 0; row < game_space[col].size(); ++row) {
+            std::cout << game_space[col][row];
+        }
+        std::cout << "\n";
+    }
+}
+
+std::vector<std::vector<char>> Init() {
+    std::cout << "initializing engine.." << "\n";
+    std::vector<std::vector<char>> space { // 11 x 9 grid is the gamespace (editable, its a vector)
+        {'#', '#', '#', '#', '#', '#', '#', '#', '#', '#', '#'}, // the # is the border. it will be immovable and solid WITH physics properties
+        {'#', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', '#'},
+        {'#', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', '#'},
+        {'#', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', '#'},
+        {'#', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', '#'},
+        {'#', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', '#'},
+        {'#', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', '#'},
+        {'#', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', '#'},
+        {'#', '#', '#', '#', '#', '#', '#', '#', '#', '#', '#'}
+    };
+    return space;
 }
