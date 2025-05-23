74d66161afe0b9ce6e6382ef78630dc2967796b0
morodirule <admin@morodirule.win>
2025-05-23 21:05:43 +0200

    Zaktualizuj plik .gitignore, aby uwzględnić pliki *.dll; zmodyfikuj makefile, aby obsługiwał kompilację dla Windows i Linux; przenieś logikę ładowania biblioteki do test.py, aby poprawić obsługę błędów i ścieżek do bibliotek.

--- makefile
+++ makefile
@@ -1,18 +1,29 @@
 # Compiler
-CXX = g++
+CXX = g++ # linux
+CXXX = x86_64-w64-mingw32-g++
 # Flags
-CXXFLAGS = -fPIC -shared
+CXXFLAGS = -fPIC -shared -static-libgcc -static-libstdc++
 # Source files
 SRCS = main.cpp bindings.cpp
 # Output file
 OUT = iTTE.so
+OUT_WIN = iTTE.dll
 
-all: $(OUT)
+# output directory
+OUT_DIR = lib
 
-$(OUT): $(SRCS)
+all: $(OUT_DIR)/$(OUT) $(OUT_DIR)/$(OUT_WIN)
+
+$(OUT_DIR):
+	mkdir -p $(OUT_DIR)
+
+$(OUT_DIR)/$(OUT): $(SRCS) | $(OUT_DIR)
 	$(CXX) $(CXXFLAGS) -o $@ $^
 
+$(OUT_DIR)/$(OUT_WIN): $(SRCS) | $(OUT_DIR)
+	$(CXXX) $(CXXFLAGS) -o $@ $^
+
 clean:
-	rm -f $(OUT)
+	rm -f $(OUT_DIR)/$(OUT) $(OUT_DIR)/$(OUT_WIN)
 
 .PHONY: all clean
--- test/test.py
+++ test/test.py
@@ -1,4 +1,25 @@
-import ctypes #<3 ctypes
-itte = ctypes.CDLL("./iTTE.so") #locate the library
+import ctypes
+import os
+import platform
+from ctypes import get_last_error
+
+parent_dir = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+lib_dir = os.path.join(parent_dir, "lib")
+try:
+    if platform.system() == "Windows":
+        dll_path = os.path.join(lib_dir, "iTTE.dll")
+        os.add_dll_directory(lib_dir)
+        kernel32 = ctypes.WinDLL('kernel32', use_last_error=True)
+        kernel32.SetDllDirectoryW(lib_dir)
+        itte = ctypes.CDLL(dll_path)
+    else:
+        dll_path = os.path.join(lib_dir, "iTTE.so")
+        os.environ['LD_LIBRARY_PATH'] = lib_dir
+        itte = ctypes.CDLL(dll_path)
+except Exception as e:
+    error_code = get_last_error()
+    print(f"Error loading DLL: {e}")
+    print(f"Last Windows error code: {error_code}")
+
 func = itte.testbind_c
 func(1000) #test functionality
