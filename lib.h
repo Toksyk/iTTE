9b3c64892f9f590a01e780a374d0cba5f4f13b6e
Toksyk <167337761+Toksyk@users.noreply.github.com>
2025-06-10 23:27:29 +0200

    Update lib.h
    Changes:
    now holds the class refrence
    publicizes base_game_space and gameObjects
    added functions from main.cpp
    readability

--- lib.h
+++ lib.h
@@ -2,23 +2,46 @@
 #define LIB_H
 #include <iostream>
 #include <vector>
+#include <memory>
+
+
+class Object {
+public:
+    int x;
+    int y;
+    char character;
+
+    Object(int x_val, int y_val, char obj_char) : x(x_val), y(y_val), character(obj_char) {
+    }
+};
+
+// Core
 
 void testbind(int x);
+void Init();
+void render();
+std::unique_ptr<Object> CreateObject(int x, int y, char character);
+void AddObject(std::unique_ptr<Object> obj);
 
-std::vector<std::vector<char>> Init();
 
-void render(const std::vector<std::vector<char>>& game_space); // const important i think
-
-std::string GetInput();
+extern std::vector<std::vector<char>> global_base_game_space;
+extern std::vector<std::unique_ptr<Object>> gameObjects;
 
 extern "C" {
+
     void testbind_c(int x);
 
-    // cant send full vector, so send the size (the pointer was already passed)
     char** Init_c(int* rows, int* cols);
-    void render_c(char** game_space_ptr, int rows, int cols);
-    void endscene_c(char** game_space_ptr, int rows); // Function to free the memory
-    char GetInput_c();
 
+    void render_c(char** game_space_ptr, int rows, int cols);
+
+    void AddObject_c(int x, int y, char character);
+
+    void MoveObject_c(int object_index, int new_x, int new_y);
+
+    char GetInput_c(); // DEPRECATED ; FOR WINDOWS ONLY
+
+    void endscene_c(char** game_space_ptr, int rows);
 }
-#endif
+
+#endif // LIB_H
